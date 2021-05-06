a57437230990970723629876730e5b1e9a385e69
os <oskinanatoliy@gmail.com>
2021-05-06 21:48:39 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a:50
+// }
+// obj.a =10;
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Tolik';
+// name = 'Alex';
+
+// {
+//     let result = 50;
+// }
+
+// console.log(result);
+
+// const obj = {
+//     name: "Tosha",
+//     age: 31,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// // answers[0] = prompt('what is ur name', '');
+// // answers[1] = prompt('what is ur surname', '');
+// // answers[2] = prompt('what is ur age', '');
+
+// // document.write(answers);
+
+// console.log(typeof(answers));
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
